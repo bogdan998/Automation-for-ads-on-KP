dd59bf4feaaf146828ad4f4235242a3c0a1af33c
Bogdan Ljubic <bogdan.ljubic98@gmail.com>
2022-01-27 13:38:15 +0100

    refactor code

--- kp.py
+++ kp.py
@@ -12,36 +12,36 @@ driver = webdriver.Chrome(PATH)
 driver.get("https://www.kupujemprodajem.com/")
 
 
-link = driver.find_element_by_xpath('//*[@id="bodyTag"]/div[9]/div/div[1]/div[2]/div/div/div/div[4]/div/span')
+link = driver.find_element(By.XPATH,'//*[@id="bodyTag"]/div[9]/div/div[1]/div[2]/div/div/div/div[4]/div/span')
 link.click()
 
 time.sleep(3)
 
-email = driver.find_element_by_id("email")
+email = driver.find_element(By.ID,"email")
 email.send_keys("magiicall8@gmail.com")
 
-passwd = driver.find_element_by_id("password")
+passwd = driver.find_element(By.ID,"password")
 passwd.send_keys("b14.....b43,,,")
 
-button = driver.find_element_by_id("submitButton")
+button = driver.find_element(By.ID,"submitButton")
 button.click()
 
 time.sleep(3)
 
-postaviteOglas = driver.find_element_by_xpath('//*[@id="leftNav"]/div[2]/a')
+postaviteOglas = driver.find_element(By.XPATH,'//*[@id="leftNav"]/div[2]/a')
 postaviteOglas.click()
 
-naslovOglasa = driver.find_element_by_id('data[group_suggest_text]')
+naslovOglasa = driver.find_element(By.ID,'data[group_suggest_text]')
 naslovOglasa.send_keys("Stephen King IT Knjiga")
 
 # time.sleep(3)
 
-kategorija = driver.find_element_by_xpath('//*[@id="categorySelection"]/div/div[1]/div/span[3]')
+kategorija = driver.find_element(By.XPATH,'//*[@id="categorySelection"]/div/div[1]/div/span[3]')
 kategorija.click()
 
 # time.sleep(3)
 
-kategorijaMeni = driver.find_element_by_xpath('//*[@id="menuGroup0"]/div[22]')
+kategorijaMeni = driver.find_element(By.XPATH,'//*[@id="menuGroup0"]/div[22]')
 kategorijaMeni.click()
 
 time.sleep(3)
@@ -56,20 +56,20 @@ grupaMeni.click()
 
 time.sleep(3)
 
-korisceno = driver.find_element_by_xpath('//*[@id="conditionForm"]/div[2]/div[1]/label[2]')
+korisceno = driver.find_element(By.XPATH,'//*[@id="conditionForm"]/div[2]/div[1]/label[2]')
 korisceno.click()
 
 # naslovOglasa = driver.find_element_by_id('data[name]')
 # naslovOglasa.clear()
 # naslovOglasa.send_keys("Promena")
 
-cena = driver.find_element_by_name('data[price]')
+cena = driver.find_element(By.NAME,'data[price]')
 cena.send_keys('1000')
 
-valuta = driver.find_element_by_xpath('//*[@id="data[currency]"]/label[1]')
+valuta = driver.find_element(By.XPATH,'//*[@id="data[currency]"]/label[1]')
 valuta.click()
 
-frame = driver.find_element_by_id('data[description]_ifr')
+frame = driver.find_element(By.ID,'data[description]_ifr')
 driver.switch_to.frame(frame)
 tekst = driver.find_element(By.ID,'tinymce')
 tekst.send_keys("Stephen King IT - stanje odlicno")
@@ -87,25 +87,26 @@ ime = driver.find_element(By.ID,'data[owner]')
 ime.clear()
 ime.send_keys('Neša')
 
-telefon = driver.find_element_by_id('phone_number')
+telefon = driver.find_element(By.ID,'phone_number')
+telefon.clear()
 telefon.send_keys('0641234567')
 
 time.sleep(60)
 
-btnSledece = driver.find_element_by_xpath('//*[@id="adFormInfo"]/div[2]/div[21]/div/input')
+btnSledece = driver.find_element(By.XPATH,'//*[@id="adFormInfo"]/div[2]/div[21]/div/input')
 btnSledece.click()
 
 time.sleep(3)
 
-chkVidljivost = driver.find_element_by_xpath('//*[@id="service-holder-none"]/div[3]/div/div[1]')
+chkVidljivost = driver.find_element(By.XPATH,'//*[@id="service-holder-none"]/div[3]/div/div[1]')
 chkVidljivost.click()
 
-btnSledece = driver.find_element_by_xpath('//*[@id="adFormPromo"]/div[4]/div/input')
+btnSledece = driver.find_element(By.XPATH,'//*[@id="adFormPromo"]/div[4]/div/input')
 btnSledece.click()
 
 time.sleep(3)
-chkPravila = driver.find_element_by_id('accept_yes')
+chkPravila = driver.find_element(By.ID,'accept_yes')
 chkPravila.click()
 
-btnPostaviteOglas = driver.find_element_by_xpath('//*[@id="adFormDeclaration"]/div[8]/div/input')
+btnPostaviteOglas = driver.find_element(By.XPATH,'//*[@id="adFormDeclaration"]/div[8]/div/input')
 btnPostaviteOglas.click()
